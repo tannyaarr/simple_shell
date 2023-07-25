41a403a644bd75da4e1c7c6ec6ff5cce5551c978
tannyaarr <tanyachiras@gmail.com>
2023-07-25 07:00:18 -0700

    shell.c, not done editing

--- exec.c
+++ exec.c
@@ -8,10 +8,12 @@
 
 void handle_setenv(shell_data *data)
 {
-	 if (data->num_args != 3) {
+	if (data->num_args != 3)
+	{
 		fprintf(stderr, "Usage: setenv VARIABLE VALUE\n");
 		return;
-				     }
+	}
+
 	if (_setenv(data->args[1], data->args[2], 1) != 0)
 		perror("setenv");
 }
@@ -24,7 +26,8 @@ void handle_setenv(shell_data *data)
 
 void handle_unsetenv(shell_data *data)
 {
-	if (data->num_args != 2) {
+	if (data->num_args != 2)
+	{
 		fprintf(stderr, "Usage: unsetenv VARIABLE VALUE\n");
 		return;
 	}
@@ -41,13 +44,14 @@ void handle_unsetenv(shell_data *data)
 
 int execute_command_child(shell_data *data, char *path)
 {
-    char *command = data->args[0];
-    char line_num_str[32]; 
-    int line_num_len = snprintf(line_num_str, sizeof(line_num_str), "%d", data->line_num);
-    char *fullpath; 
+	char *command = data->args[0];
+	char line_num_str[32];
+	int line_num_len = snprintf(line_num_str, sizeof(line_num_str), "%d", data->line_num);
+	char *fullpath; 
 
 
-    if (_strcmp(data->args[0], "echo") == 0) {
+    if (_strcmp(data->args[0], "echo") == 0)
+    {
         handle_echo_command(data);
     } 
     else 
@@ -73,7 +77,8 @@ int execute_command_child(shell_data *data, char *path)
                 write(STDERR_FILENO, ": command not found\n", 20);
                 return (127);
         }
-        if (data->line != NULL && data->line[0] != '\0') {
+        if (data->line != NULL && data->line[0] != '\0')
+	{
             execve(fullpath, data->args, environ);
 	    exit_with_error(data, "%s: No such file or directory\n", command);
 	    return (127);
@@ -91,21 +96,21 @@ int execute_command_child(shell_data *data, char *path)
 
 void exit_with_error(shell_data *data, const char *format, ...)
 {
-    va_list args;
-    char buffer[4096]; 
-    int len;
-    char line_num_buffer[32];
-    int line_num_len;
-
-    va_start(args, format);
-    len = vsnprintf(buffer, sizeof(buffer), format, args);
-    va_end(args);
-
-    write(STDERR_FILENO, data->program_name, _strlen(data->program_name));
-    write(STDERR_FILENO, ": line ", 7);
-    line_num_len = snprintf(line_num_buffer, sizeof(line_num_buffer), "%d", data->line_num);
-    write(STDERR_FILENO, line_num_buffer, line_num_len);
-    write(STDERR_FILENO, ": ", 2);
-    write(STDERR_FILENO, buffer, len);
-    exit(127);
+	va_list args;
+	char buffer[4096];
+	int len;
+	char line_num_buffer[32];
+	int line_num_len;
+	
+	va_start(args, format);
+	len = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+	
+	write(STDERR_FILENO, data->program_name, _strlen(data->program_name));
+	write(STDERR_FILENO, ": line ", 7);
+	line_num_len = snprintf(line_num_buffer, sizeof(line_num_buffer), "%d", data->line_num);
+	write(STDERR_FILENO, line_num_buffer, line_num_len);
+	write(STDERR_FILENO, ": ", 2);
+	write(STDERR_FILENO, buffer, len);
+	exit(127);
 }
--- shell.c
+++ shell.c
@@ -7,50 +7,62 @@
  * Return: void
  */
 
-void run_shell_command(shell_data *data) {
-    pid_t pid;
-    int exit_status;
-    int child_exit_status;
+void run_shell_command(shell_data *data)
+{
+	pid_t pid;
+	int exit_status;
+	int child_exit_status;
 
-    if (data->args[0] == NULL) {
-        return;
-    }
-    if (_strcmp(data->args[0], "exit") == 0)
-        exit(0);
-    else if (_strcmp(data->args[0], "env") == 0)
-        print_env();
-    else if (_strcmp(data->args[0], "setenv") == 0)
-    {
-        handle_setenv(data);
-    }
-    else if (_strcmp(data->args[0], "unsetenv") == 0)
-    {
-        handle_unsetenv(data);
-    }
-    else if (_strcmp(data->args[0], "cd") == 0)
-        cd_command(data);
-    else if (_strcmp(data->args[0], "echo") == 0)
-        handle_echo_command(data);
-    else {
-        pid = fork();
-
-        if (pid == -1) {
-            exit_with_error(data, "Fork error");
-        } else if (pid == 0) {
-            exit_status = execute_command_child(data, get_command_path(data));
-	    exit(exit_status);
-        } else {
-            waitpid(pid, &exit_status, 0);
-            if (WIFEXITED(exit_status)) {
-                child_exit_status = WEXITSTATUS(exit_status);
-                if (child_exit_status != 0) {
-                    errno = child_exit_status;
-                }
-            } else {
-                errno = 127;
-            }
-        }
-    }
+	if (data->args[0] == NULL)
+	{
+		return;
+	}
+	if (_strcmp(data->args[0], "exit") == 0)
+		exit(0);
+	else if (_strcmp(data->args[0], "env") == 0)
+		print_env();
+	else if (_strcmp(data->args[0], "setenv") == 0)
+	{
+		handle_setenv(data);
+	}
+	else if (_strcmp(data->args[0], "unsetenv") == 0)
+	{
+		handle_unsetenv(data);
+	}
+	else if (_strcmp(data->args[0], "cd") == 0)
+		cd_command(data);
+	else if (_strcmp(data->args[0], "echo") == 0)
+		handle_echo_command(data);
+	else
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			exit_with_error(data, "Fork error");
+		}
+		
+		else if (pid == 0)
+		{
+			exit_status = execute_command_child(data, get_command_path(data));
+			exit(exit_status);
+		}
+		else
+		{
+			waitpid(pid, &exit_status, 0);
+			if (WIFEXITED(exit_status))
+			{
+				child_exit_status = WEXITSTATUS(exit_status);
+				if (child_exit_status != 0)
+				{
+					errno = child_exit_status;
+				}
+			}
+			else
+			{
+				errno = 127;
+			}
+		}
+	}
 }
 
 /**
@@ -60,47 +72,56 @@ void run_shell_command(shell_data *data) {
  * Return: 0 on success, otherwise an error code.
  */
 
-int main(int argc, char **argv) {
-    int interactive_mode;
-    shell_data data;
-    init_shell_data(&data);
+int main(int argc, char **argv)
+{
+	int interactive_mode;
+	shell_data data;
+	init_shell_data(&data);
+	
+	if (argc > 1)
+	{
+		data.program_name = argv[0];
+		run_file_command(argv[0], argv[1], &data);
+	}
+	else
+	{
+		data.program_name = argv[0];
+		interactive_mode = isatty(STDIN_FILENO);
+		if (interactive_mode)
+		{
+			while (1)
+			{
+				printf("($) ");
+				fflush(stdout);
 
-    if (argc > 1) {
-        data.program_name = argv[0];
-        run_file_command(argv[0], argv[1], &data);
-    } else {
-        data.program_name = argv[0];
-        interactive_mode = isatty(STDIN_FILENO);
-        if (interactive_mode) {
-            while (1) {
-                printf("($) ");
-                fflush(stdout);
-
-                if (read_shell_input(&data) == -1) {
-                    free_shell_data(&data);
-                    exit(EXIT_FAILURE);
+                if (read_shell_input(&data) == -1)
+		{
+			free_shell_data(&data);
+			exit(EXIT_FAILURE);
                 }
 
-                if (data.line == NULL) {
-                    break;
+                if (data.line == NULL)
+		{
+			break;
                 }
 
                 run_shell_command(&data);
-            }
-        } else {
-            while (read_shell_input(&data) == 0) {
-                run_shell_command(&data);
-            }
-	    if (errno > 100)
-		    exit(errno);
-        }
-    }
+			}
+		}
+		else
+		{
+			while (read_shell_input(&data) == 0)
+			{
+				run_shell_command(&data);
+			}
+			if (errno > 100)
+				exit(errno);
+		}
+	}
 
-    free_shell_data(&data);
-    return 0;
+	free_shell_data(&data);
+	return 0;
 }
-
-
 
 /**
  * run_file_command - File command function
@@ -111,27 +132,27 @@ int main(int argc, char **argv) {
 
 void run_file_command(const char *program_name, const char *file_name, shell_data *data)
 {
-    FILE *file;
-    char line[READ_BUF_SIZE];
+	FILE *file;
+	char line[READ_BUF_SIZE];
 
-    file = fopen(file_name, "r");
-    if (file == NULL)
-    {
-        perror(program_name);
-        exit(EXIT_FAILURE);
-    }
-
-    while (fgets(line, sizeof(line), file) != NULL)
-    {
-        line[strcspn(line, "\n")] = '\0';
-        data->line = strdup(line);
-        tokenize(data);
-        run_shell_command(data);
-	free(data->line);
-	data->line = NULL;
-    }
-
-    fclose(file);
+	file = fopen(file_name, "r");
+	if (file == NULL)
+	{
+		perror(program_name);
+		exit(EXIT_FAILURE);
+	}
+	
+	while (fgets(line, sizeof(line), file) != NULL)
+	{
+		line[strcspn(line, "\n")] = '\0';
+		data->line = strdup(line);
+		tokenize(data);
+		run_shell_command(data);
+		free(data->line);
+		data->line = NULL;
+	}
+	
+	fclose(file);
 }
 
 /**
@@ -140,13 +161,13 @@ void run_file_command(const char *program_name, const char *file_name, shell_dat
  * Return: 0 if successful, -1 on error or end of input
  */
 
-
 int read_shell_input(shell_data *data)
 {
-    ssize_t chars_read;
-    size_t input_size = READ_BUF_SIZE;
-
-    if (data->line != NULL) {
+	ssize_t chars_read;
+	size_t input_size = READ_BUF_SIZE;
+	
+	if (data->line != NULL)
+	{
         free(data->line);
         data->line = NULL;
     }
