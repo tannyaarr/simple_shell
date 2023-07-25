565560230bc24042a942f99802635ae6cc40d425
tannyaarr <tanyachiras@gmail.com>
2023-07-25 05:03:17 -0700

    updated and fixed betty error in cmd

--- cmd.c
+++ cmd.c
@@ -1,35 +1,52 @@
 #include "shell.h"
 
-
+/**
+ * handle_echo_command - handles echo command
+ * @data: a pointer to the data to be processed
+ * Return: the results of the processing
+ */
 
 void handle_echo_command(shell_data *data)
 {
-    int i;
-    for (i = 1; i < data->num_args; i++) {
-        printf("%s", data->args[i]);
-        if (i != data->num_args - 1) {
-            printf(" ");
-        }
-    }
-    printf("\n");
-    if (data->line != NULL) {
-        free(data->line);
-        data->line = NULL;
-    }
+	int i;
+
+	for (i = 1; i < data->num_args; i++)
+	{
+		printf("%s", data->args[i]);
+
+		if (i != data->num_args - 1)
+		{
+			printf(" ");
+		}
+	}
+	printf("\n");
+
+	if (data->line != NULL)
+	{
+		free(data->line);
+		data->line = NULL;
+	}
 
 }
 
-char* _getenv(const char* name) {
-    extern char** environ;
-    int nameLen = _strlen(name);
-    int i;
+/**
+ * _getenv- gets environment
+ * @name: name of environment to be printed
+ */
 
-    for (i = 0; environ[i] != NULL; i++) {
-        if (_strncmp(name, environ[i], nameLen) == 0 && environ[i][nameLen] == '=') 
-        {
-            return (environ[i] + nameLen + 1);
-        }
-    }
+char *_getenv(const char *name)
+{
+	char **environ;
+	int nameLen = _strlen(name);
+	int i;
 
-    return (NULL);
+	for (i = 0; environ[i] != NULL; i++)
+	{
+		if (_strncmp(name, environ[i], nameLen) == 0 && environ[i][nameLen] == '=')
+		{
+			return (environ[i] + nameLen + 1);
+		}
+	}
+
+	return (NULL);
 }
