ca7e3be12f48190456357ca1c7912a106e8e8afe
tannyaarr <tanyachiras@gmail.com>
2023-07-25 14:39:54 -0700

    betty fixed for getline.c

--- getline.c
+++ getline.c
@@ -28,45 +28,45 @@ int append_to_line(shell_data *data, char *input,
 
 ssize_t _getline(shell_data *data)
 {
-    char input[READ_BUF_SIZE];
-    ssize_t chars_read = 0, i, total_chars_read = 0;
+	char input[READ_BUF_SIZE];
+	ssize_t chars_read = 0, i, total_chars_read = 0;
 
-    while (1)
-    {
-        chars_read = read(STDIN_FILENO, input, READ_BUF_SIZE);
-        if (chars_read == -1)
-        {
-            if (errno == EINTR)
-                continue;
-            else
-                return (-1);
-        }
-        else if (chars_read == 0)
-            break;
+	while (1)
+	{
+		chars_read = read(STDIN_FILENO, input, READ_BUF_SIZE);
+		if (chars_read == -1)
+		{
+			if (errno == EINTR)
+				continue;
+			else
+				return (-1);
+		}
+		else if (chars_read == 0)
+			break;
 
-        for (i = 0; i < chars_read; i++)
-        {
-            if (input[i] == '\n')
-            {
-                if (append_to_line(data, input, i, &total_chars_read) == -1)
-                    return (-1);
+		for (i = 0; i < chars_read; i++)
+		{
+			if (input[i] == '\n')
+			{
+				if (append_to_line(data, input, i, &total_chars_read) == -1)
+					return (-1);
 
-                tokenize(data); 
-                return (total_chars_read);
-            }
-        }
+				tokenize(data);
+				return (total_chars_read);
+			}
+		}
 
-        if (append_to_line(data, input, chars_read, &total_chars_read) == -1)
-            return (-1);
-    }
+		if (append_to_line(data, input, chars_read, &total_chars_read) == -1)
+			return (-1);
+	}
 
-    if (total_chars_read > 0)
-    {
-        data->line = realloc(data->line, total_chars_read + 1);
-        if (data->line == NULL)
-            return (-1);
-        data->line[total_chars_read] = '\0';
-    }
+	if (total_chars_read > 0)
+	{
+		data->line = realloc(data->line, total_chars_read + 1);
+		if (data->line == NULL)
+			return (-1);
+		data->line[total_chars_read] = '\0';
+	}
 
-    return (total_chars_read);
+	return (total_chars_read);
 }
